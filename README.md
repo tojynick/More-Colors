221b3351439ee0da857d9446243e3167a3e8ec72
Kai Fardreamer <tojynick@protonmail.com>
2024-09-01 22:48:57 +0300

    Addon info setup

---
+++ README.md
@@ -0,0 +1,12 @@
+<img alt="Addon banner" src="./Resources/Banner.png"/>
+
+# 🖌️ More Colors!
+Blender addon to make vertex painting easier!
+
+## ✅ Prerequisites
+-   [Blender 4.0 or higher](https://www.blender.org/download/)
+
+## 🔧 Installation
+
+## 📃 License
+Licensed under GNU General Public License, Version 3.0, ([LICENSE](LICENSE) or https://www.gnu.org/licenses/gpl-3.0.en.html)
---
+++ __init__.py
@@ -0,0 +1,17 @@
+bl_info = {
+    "name": "More Colors!",
+    "author": "tojynick",
+    "description": "A set of tools to make vertex painting easier.",
+    "blender": (4, 0, 0),
+    "version": (1, 0, 0),
+    "location": "",
+    "tracker_url": "https://github.com/tojynick/More-Colors",
+    "support": "COMMUNITY",
+    "category": "Paint"
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
